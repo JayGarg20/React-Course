b6b4978f49a2b4673f7d02e685aeb233388cf0bb
Jay Garg <gargjay20@gmail.com>
2026-07-15 22:54:14 +0530

    learn how to make new components

--- lec1viteReact/src/App.jsx
+++ lec1viteReact/src/App.jsx
@@ -1,8 +1,14 @@
+import Chai from './chai'
+
 
 function App() {
   return (
-    <h1>Hello World</h1>
-      
+    <>
+      <Chai />
+      <h1>App is ready</h1>
+      <p>App is ready</p>
+    </>
+
   )
 }
 
