e4625adbc911fe29d4943bd2edf8b082003cdbb3
Jay Garg <gargjay20@gmail.com>
2026-07-15 23:49:22 +0530

    creating own finctions and components

--- lec1viteReact/src/App.jsx
+++ lec1viteReact/src/App.jsx
@@ -2,11 +2,13 @@ import Chai from './chai'
 
 
 function App() {
+  const userName = 'Jay Garg'
   return (
     <>
       <Chai />
       <h1>App is ready</h1>
-      <p>App is ready</p>
+      <p>Welcome, {userName}!</p>
+      {/* <p>Welcome, {anotherUser}!</p> */}
     </>
 
   )
--- lec1viteReact/src/main.jsx
+++ lec1viteReact/src/main.jsx
@@ -1,10 +1,47 @@
-// import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 
-import App from './App.jsx'
+import App from './App.jsx';
 
+// eslint-disable-next-line react-refresh/only-export-components
+function MyApp() {
+    return (
+        <div>
+            <h1>Hello React</h1>
+        </div>
+    )
+}
+// const reactElement = {
+//     type : 'a',
+//     props:{
+//         href : 'https://www.google.com',
+//         target: '_blank'
+//     },
+//     children : 'Google'
+// }
+const anotherElement = (
+    <a href="https://www.google.com" target="_blank">
+        Google
+    </a>
+);
+const anotheruser = "ChaiAurReact";
+// eslint-disable-next-line no-undef
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://www.google.com',
+        target: '_blank'
+    },
+    'Google', anotheruser
+);
 createRoot(document.getElementById('root')).render(
-  
+    <>
     <App />
-
+    <MyApp />
+    {anotherElement}
+    {reactElement}
+    </>
 )
+
+
+
